c20a9a02f0626b1b6e46ef4417f2e67e6451da3e
N0tR1CH <damianrichter58@gmail.com>
2023-08-13 23:16:26 +0200

    Add typing into the boxes and backspacing

--- word_masters.js
+++ word_masters.js
@@ -1,68 +1,93 @@
 /* Word masters - Wordle clone js file */
-const GET_WORD_URL = "https://words.dev-apis.com/word-of-the-day"
-const VALIDATE_WORD_URL = "https://words.dev-apis.com/validate-word"
-const wordsEl = document.querySelectorAll('.word')
+const GET_WORD_URL = "https://words.dev-apis.com/word-of-the-day";
+const VALIDATE_WORD_URL = "https://words.dev-apis.com/validate-word";
+const wordsEl = document.querySelectorAll(".word");
 
 /* Object describing the current cursor position */
-let currentCursorPosition = { currentWordIndex:  0, currentWordPosition: 0 }
+let currentCursorPosition = { currentWordIndex: 0, currentWordPosition: 0 };
 
 /* Word of the day */
 let wordOfTheDay;
 
 /* Checking if user input is right */
 function isLetter(letter) {
-    return /^[a-zA-Z]$/.test(letter);
+  return /^[a-zA-Z]$/.test(letter);
 }
 
 /* Getting response from word-of-the-day API */
 async function getWordOfTheDay() {
-    try {
-        const promise = await fetch(GET_WORD_URL);
-        const processedResponse = await promise.json();
-        return processedResponse.word;
-    } catch {
-        alert(`Couldn't get the word of the day from ${GET_WORD_URL} API :/`)
-        console.error('Error:', error);
-    }
+  try {
+    const promise = await fetch(GET_WORD_URL);
+    const processedResponse = await promise.json();
+    return processedResponse.word;
+  } catch {
+    alert(`Couldn't get the word of the day from ${GET_WORD_URL} API :/`);
+    console.error("Error:", error);
+  }
 }
 
 /* Keypress handling */
 function enableKeypressHandling() {
-    document.addEventListener('keypress', handleKeyPress);
-    document.addEventListener('keydown', handleBackSpaceAndEnter);
+  document.addEventListener("keypress", handleKeyPress);
+  document.addEventListener("keydown", handleBackSpaceAndEnter);
 }
 
 function handleKeyPress(event) {
-    let keyName = event.key;
+  if (currentCursorPosition.currentWordPosition == 5) {
+    return;
+  }
 
-    if (!isLetter(keyName)) {
-        return;
-    }
+  let keyName = event.key;
 
-    keyName = keyName.toUpperCase();
-    console.log(keyName);
+  if (!isLetter(keyName)) {
+    return;
+  }
+
+  keyName = keyName.toUpperCase();
+
+  wordsEl[currentCursorPosition.currentWordIndex].children[
+    currentCursorPosition.currentWordPosition++
+  ].innerText = keyName;
+
+  console.log(keyName);
+  console.log(currentCursorPosition);
 }
 
 function handleBackSpaceAndEnter(event) {
-    let keyName = event.key;
+  let keyName = event.key;
 
-    switch(keyName) {
-        case "Backspace":
-            console.log("You got backspace babe");
-            break;
-        case "Enter":
-            console.log("You got enter babe");
-            break;
-        default:
-            return;
-    }
+  switch (keyName) {
+    case "Backspace":
+      handleBackSpace();
+      break;
+    case "Enter":
+      console.log("You got enter babe");
+      if (currentCursorPosition.currentWordPosition != 5) {
+        return;
+      }
+      break;
+    default:
+      return;
+  }
+}
+
+function handleBackSpace() {
+  if(currentCursorPosition.currentWordPosition == 0) {
+    return;
+  }
+
+  wordsEl[currentCursorPosition.currentWordIndex].children[
+    --currentCursorPosition.currentWordPosition
+  ].innerText = '';
+
+  console.log(currentCursorPosition.currentWordPosition);
 }
 
 /* Main Function */
 async function init() {
-    wordOfTheDay = await getWordOfTheDay();
+  wordOfTheDay = await getWordOfTheDay();
 
-    enableKeypressHandling();
+  enableKeypressHandling();
 }
 
 init();
