7dac875a3f596d707b33978c7d0e0624efa70d92
N0tR1CH <damianrichter58@gmail.com>
2023-08-14 23:30:12 +0200

    Add word existence validation and some animations

--- word_masters.js
+++ word_masters.js
@@ -2,6 +2,10 @@
 const GET_WORD_URL = "https://words.dev-apis.com/word-of-the-day";
 const VALIDATE_WORD_URL = "https://words.dev-apis.com/validate-word";
 const wordsEl = document.querySelectorAll(".word");
+const loadingSymbol = document.querySelector(".loading-symbol");
+
+/* Current user word */
+let currentUserWord = '';
 
 /* Object describing the current cursor position */
 let currentCursorPosition = { currentWordIndex: 0, currentWordPosition: 0 };
@@ -22,6 +26,21 @@ async function getWordOfTheDay() {
     return processedResponse.word;
   } catch {
     alert(`Couldn't get the word of the day from ${GET_WORD_URL} API :/`);
+    console.error("Error:", error);
+  }
+}
+
+/* Validate word */
+async function isWordValid() {
+  try {
+    const promise = await fetch(VALIDATE_WORD_URL, {
+      method: 'POST',
+      body: JSON.stringify({ "word": currentUserWord })
+    })
+    const processedResponse = await promise.json();
+    return processedResponse.validWord;
+  } catch {
+    alert(`Couldn't validate the word using ${VALIDATE_WORD_URL} API :/`);
     console.error("Error:", error);
   }
 }
@@ -49,8 +68,11 @@ function handleKeyPress(event) {
     currentCursorPosition.currentWordPosition++
   ].innerText = keyName;
 
+  currentUserWord += keyName;
+
   console.log(keyName);
   console.log(currentCursorPosition);
+  console.log(currentUserWord);
 }
 
 function handleBackSpaceAndEnter(event) {
@@ -65,6 +87,7 @@ function handleBackSpaceAndEnter(event) {
       if (currentCursorPosition.currentWordPosition != 5) {
         return;
       }
+      handleEnter();
       break;
     default:
       return;
@@ -80,7 +103,40 @@ function handleBackSpace() {
     --currentCursorPosition.currentWordPosition
   ].innerText = '';
 
+  currentUserWord = currentUserWord.slice(0, -1);
+
   console.log(currentCursorPosition.currentWordPosition);
+}
+
+async function handleEnter() {
+  if (currentCursorPosition.currentWordPosition != 5) {
+    return;
+  }
+
+  loadingSymbol.classList.remove('loading-off');
+
+  const result = await isWordValid();
+
+  console.log(result);
+
+  if (!result) {
+    handleWrongResult();
+
+    return;
+  }
+
+  loadingSymbol.classList.add('loading-off');
+}
+
+function handleWrongResult() {
+  loadingSymbol.classList.add('loading-off');
+
+  for (let i = 0; i < 5; i++) {
+    wordsEl[currentCursorPosition.currentWordIndex].children[i].classList.add('red-border');
+    setTimeout(() => {
+      wordsEl[currentCursorPosition.currentWordIndex].children[i].classList.remove('red-border');
+    }, 1000)
+  }
 }
 
 /* Main Function */
